82fca83cecfec922c693f69626dd2e908a3befb9
kirillEN <81442482+kixxpy@users.noreply.github.com>
2023-11-17 15:43:20 +0300

    add loginsiup

--- src/pages/LoginSigup.jsx
+++ src/pages/LoginSigup.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import styles from './css/LoginSigup.module.css';
 export const LoginSigup = () => {
 	return (
-		<div>LoginSigup</div>
-	)
-}
+		<div className={styles.loginsgiup}>
+			<div className={styles.container}>
+				<h1>Sing Up</h1>
+				<div className={styles.fields}>
+					<input type="text" placeholder="Your Name" />
+					<input type="email" placeholder="Email Address" />
+					<input type="password" placeholder="Password" />
+				</div>
+				<button>Continue</button>
+				<p className={styles.login}>
+					Already have an account? <span>Login here</span>
+				</p>
+				<div className={styles.agree}>
+					<input type="checkbox" name="" id="" />
+					<p>By continuing, i aggre to the terms of use & privacy polis.</p>
+				</div>
+			</div>
+		</div>
+	);
+};
