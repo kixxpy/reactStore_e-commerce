fd73a26550d5f5c29eb02debe93b3b66c5eef795
kirillEN <81442482+kixxpy@users.noreply.github.com>
2023-11-20 14:30:51 +0300

    add product-display

--- src/components/Beadcrum/Beadcrum.jsx
+++ src/components/Beadcrum/Beadcrum.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './Beadcrum.module.css';
-import arrow_icon from '../assets/arrow.png';
+import arrow_icon from '../assets/breadcrum_arrow.png';
 
 export const Beadcrum = (props) => {
-	const {product} = props;
+	const { product } = props;
 	return (
 		<div className={styles.beadcrum}>
-			HOME <img src={arrow_icon} alt="arrow_icon" /> 
+			HOME <img src={arrow_icon} alt="arrow_icon" />
 			SHOP <img src={arrow_icon} alt="arrow_icon" />
 			{product.category} <img src={arrow_icon} alt="arrow_icon" />
 			{product.name}
 		</div>
-	)
-}
+	);
+};
--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -4,13 +4,12 @@ import star_icon from '../assets/star_icon.png';
 import star_dull_icon from '../assets/star_dull_icon.png';
 
 export const ProductDisplay = (props) => {
-	const {product} = props;
+	const { product } = props;
 
 	return (
 		<div className={styles.productdisplay}>
 			<div className={styles.left}>
 				<div className={styles.img_list}>
-					<img src={product.image} alt="" />
 					<img src={product.image} alt="" />
 					<img src={product.image} alt="" />
 					<img src={product.image} alt="" />
@@ -21,7 +20,7 @@ export const ProductDisplay = (props) => {
 			</div>
 			<div className={styles.right}>
 				<h1>{product.name}</h1>
-				<div>
+				<div className={styles.stars}>
 					<img className={styles.star} src={star_icon} alt="" />
 					<img className={styles.star} src={star_icon} alt="" />
 					<img className={styles.star} src={star_icon} alt="" />
@@ -29,7 +28,27 @@ export const ProductDisplay = (props) => {
 					<img className={styles.star} src={star_dull_icon} alt="" />
 					<p>(122)</p>
 				</div>
+				<div className={styles.price}>
+					<div className={styles.old_price}>${product.old_price}</div>
+					<div className={styles.new_price}>${product.new_price}</div>
+				</div>
+				<div className={styles.description}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus officia incidunt quas, vitae sit temporibus. Deserunt aspernatur ex mollitia velit, dicta perferendis consequuntur provident molestiae illo officia! Quaerat, quisquam illo!
+				</div>
+				<div className={styles.size}>
+					<h1>Select Size</h1>
+					<div className={styles.sizes}>
+						<div>S</div>
+						<div>M</div>
+						<div>L</div>
+						<div>XL</div>
+						<div>XXL</div>
+					</div>
+				</div>
+				<button>ADD TO CART</button>
+				<p className={styles.category}><span>Category: </span>Women, T-Shirt, Crop Top</p>
+				<p className={styles.category}><span>Tags: </span>Modern, Latest</p>
 			</div>
 		</div>
-	)
-}
+	);
+};
